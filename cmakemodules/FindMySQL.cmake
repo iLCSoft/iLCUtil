72fcca57f702d47ed09162f79ff15d5edce83d3b
Jan Engels <jan.engels@desy.de>
2011-01-27 12:37:42 +0000

    expanded PATHS to fix CMAKE_FIND_ROOT_PATH problem

--- cmakemodules/FindMySQL.cmake
+++ cmakemodules/FindMySQL.cmake
@@ -15,46 +15,73 @@ IF( NOT DEFINED MySQL_DIR AND DEFINED MySQL_HOME )
     SET( MySQL_DIR "${MySQL_HOME}" )
 ENDIF( NOT DEFINED MySQL_DIR AND DEFINED MySQL_HOME )
 
-IF( NOT MySQL_FIND_QUIETLY )
-    MESSAGE( STATUS "Check for MySQL: ${MySQL_DIR}" )
-ENDIF( NOT MySQL_FIND_QUIETLY )
 
-# include dirs
+
+# ------------- include dirs ---------------------------------
 SET( MySQL_INCLUDE_DIRS MySQL_INCLUDE_DIRS-NOTFOUND )
-FIND_PATH( MySQL_INCLUDE_DIRS
-    NAMES mysql.h
-    PATHS ${MySQL_DIR}
+MARK_AS_ADVANCED( MySQL_INCLUDE_DIRS )
+
+# WARNING: if using CMAKE_FIND_ROOT_PATH to define the installation path of mysql
+#           the following FIND_PATH commando finds the wrong mysql installation because PATH_SUFFIXES is used ... !?
+#           if PATHS are expanded or CMAKE_PREFIX_PATH is used instead of CMAKE_FIND_ROOT_PATH the problem goes away
+#FIND_PATH( MySQL_INCLUDE_DIRS NAMES mysql.h PATHS ${MySQL_DIR} PATH_SUFFIXES include include/mysql include/mysql5/mysql NO_DEFAULT_PATH )
+FIND_PATH( MySQL_INCLUDE_DIRS NAMES mysql.h PATHS ${MySQL_DIR}/include ${MySQL_DIR}/include/mysql ${MySQL_DIR}/include/mysql5/mysql NO_DEFAULT_PATH )
+
+# if not found in MySQL_DIR / CMAKE_PREFIX_PATH / CMAKE_INCLUDE_PATH or CMAKE_FIND_ROOT_PATH ...
+# look into the following locations
+FIND_PATH( MySQL_INCLUDE_DIRS NAMES mysql.h PATHS
     /usr
     /usr/local
     /opt/local
     /opt/products/mysql/5.0.45
     /opt/products/mysql/5.0.26
     PATH_SUFFIXES include include/mysql include/mysql5/mysql
+)
+
+
+
+
+# ------------- libraries ------------------------------------
+SET( MySQL_LIBRARIES MySQL_LIBRARIES-NOTFOUND )
+MARK_AS_ADVANCED( MySQL_LIBRARIES )
+
+# WARNING: see WARNING for MySQL_INCLUDE_DIRS above !
+#FIND_LIBRARY( MySQL_LIBRARIES NAMES mysqlclient mysqlclient_r PATHS ${MySQL_DIR}
+#    PATH_SUFFIXES lib64 lib64/mysql lib64/mysql5/mysql lib lib/mysql lib/mysql5/mysql
+#    NO_DEFAULT_PATH
+#)
+
+FIND_LIBRARY( MySQL_LIBRARIES NAMES mysqlclient mysqlclient_r PATHS
+    ${MySQL_DIR}/lib64
+    ${MySQL_DIR}/lib64/mysql
+    ${MySQL_DIR}/lib64/mysql5/mysql
+    ${MySQL_DIR}/lib
+    ${MySQL_DIR}/lib/mysql
+    ${MySQL_DIR}/lib/mysql5/mysql
     NO_DEFAULT_PATH
 )
 
-# libraries
-SET( MySQL_LIBRARIES MySQL_LIBRARIES-NOTFOUND )
-FIND_LIBRARY( MySQL_LIBRARIES
-    NAMES mysqlclient mysqlclient_r
-    PATHS ${MySQL_DIR}
+# if not found in MySQL_DIR / CMAKE_PREFIX_PATH / CMAKE_LIBRARY_PATH or CMAKE_FIND_ROOT_PATH ...
+# look into the following locations:
+FIND_LIBRARY( MySQL_LIBRARIES NAMES mysqlclient mysqlclient_r PATHS
     /usr
     /usr/local
     /opt/local
     /opt/products/mysql/5.0.45
     /opt/products/mysql/5.0.26
     PATH_SUFFIXES lib64 lib64/mysql lib64/mysql5/mysql lib lib/mysql lib/mysql5/mysql
-    NO_DEFAULT_PATH
 )
 IF( MySQL_LIBRARIES )
     GET_FILENAME_COMPONENT( MySQL_LIBRARY_DIRS ${MySQL_LIBRARIES} PATH )
+    MARK_AS_ADVANCED( MySQL_LIBRARY_DIRS )
 ENDIF( MySQL_LIBRARIES )
-MARK_AS_ADVANCED( MySQL_INCLUDE_DIRS MySQL_LIBRARY_DIRS MySQL_LIBRARIES )
+
+
 
 # ---------- final checking ---------------------------------------------------
 INCLUDE( FindPackageHandleStandardArgs )
 # set MySQL_FOUND to TRUE if all listed variables are TRUE and not empty
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( MySQL DEFAULT_MSG MySQL_INCLUDE_DIRS MySQL_LIBRARIES )
 
-# FIXME DEPRECATED
 SET( MySQL_FOUND ${MYSQL_FOUND} )
+
