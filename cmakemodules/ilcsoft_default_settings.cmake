69d7be950672eaeeb725e2a3885eae10470c28d2
Jan Engels <jan.engels@desy.de>
2011-01-28 14:34:39 +0000

    re-activated MacroInstallSharedLibrary

--- cmakemodules/ilcsoft_default_settings.cmake
+++ cmakemodules/ilcsoft_default_settings.cmake
@@ -7,6 +7,7 @@ SET( CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS TRUE ) # default in cmake 2.6
 
 # include helper macros
 INCLUDE( MacroAddSharedLibrary )
+INCLUDE( MacroInstallSharedLibrary )
 INCLUDE( MacroInstallDirectory )
 INCLUDE( MacroDisplayStandardVariables )
 INCLUDE( MacroGeneratePackageConfigFiles )
