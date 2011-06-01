52443ac855855794193275d05417cb974304ce6e
Jan Engels <jan.engels@desy.de>
2011-06-01 06:39:16 +0000

    updated tools versions

--- cmakemodules/ilcsoft_versions_v01-11.cmake
+++ cmakemodules/ilcsoft_versions_v01-11.cmake
@@ -15,38 +15,38 @@ set( geant4_version                 "9.3.p02"   )
 
 # ============ ilcsoft core packages ================
 set( lcio_version                   "tags/v01-51-02"        )
-set( ilcutil_version                "branches/v00-01-pre02" )
+set( ilcutil_version                "branches/v00-01"       )
 set( ced_version                    "tags/v01-02"           )
 set( gear_version                   "tags/v00-17"           )
 set( conddbmysql_version            "tags/CondDBMySQL_ILC-0-9-3" )
 set( lccd_version                   "tags/v01-02"           )
 set( raida_version                  "tags/v01-06-01"        )
 set( marlin_version                 "tags/v01-00"           )
-set( marlinutil_version             "tags/v01-02"           )
+set( marlinutil_version             "tags/v01-03"           )
 set( kaltest_version                "tags/v01-01-01"        )
 set( kaldet_version                 "tags/v01-01-01"        )
 
-set( mokka_version                  "tags/mokka-07-06-p01"  )
+set( mokka_version                  "tags/mokka-07-06-p02"  )
 set( druid_version                  "tags/1.8"              )
 
 # ============ ilcsoft marlin plugins ===============
-set( marlinreco_version             "trunk"                 )
+set( marlinreco_version             "tags/v00-20"           )
 set( cedviewer_version              "tags/v01-02"           )
-set( overlay_version                "tags/v00-09"           )
-set( lcfivertex_version             "branches/v00-05-pre"   )
+set( overlay_version                "tags/v00-10"           )
+set( lcfivertex_version             "branches/v00-05"       )
 set( garlic_version                 "tags/v2.0.4"           )
-set( pandorapfanew_version          "tags/v00-05"           )
-set( marlinpandora_version          "tags/v00-04"           )
-#set( pandoraanalysis_version       "trunk"                 )
+set( pandorapfanew_version          "tags/v00-06"           )
+set( marlinpandora_version          "tags/v00-05"           )
+set( pandoraanalysis_version        "tags/v00-02"           )
 #set( pandoramonitoring_version     "trunk"                 )
 #set( pandorapfa_version            "trunk"                 ) # DEPRECATED
 #set( silicondigi_version           "trunk"                 ) # DEPRECATED
 set( fastjetclustering_version      "tags/v00-02"           )
 set( marlintpc_version              "tags/v00-08-04"        )
-set( eutelescope_version            "branches/v00-06-00-pre")
+set( eutelescope_version            "tags/v00-05-03"        )
 
 # ============ ilcsoft config packages ==============
-set( standardconfig_version         "trunk"         )
+set( standardconfig_version         "tags/v03-00"   )
 set( mokkadbconfig_version          "tags/v02-02"   )
 set( lcfimokkabasednets_version     "tags/v00-01"   )
 
