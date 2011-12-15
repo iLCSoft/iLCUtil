fe36183bb4c2d8772c9f00c3420844d721e7e054
Jan Engels <jan.engels@desy.de>
2011-12-15 14:11:51 +0000

    added v01-13-01 (still in preparation)

---
+++ cmakemodules/ilcsoft_versions_v01-13-01.cmake
@@ -0,0 +1,15 @@
+# package versions from the ilcsoft release v01-13-01
+# this file should not be changed!
+
+include( "ilcsoft_versions_v01-13" )
+
+# ===== external tools ================
+#set( geant4_version                 "9.4.p02"   )
+
+# ============ ilcsoft core packages ================
+set( kaldet_version                 "branches/v01-04-pre00" )
+set( marlintrk_version              "branches/v01-02-pre00" )
+set( marlintrkprocessors_version    "branches/v01-01-pre00" )
+
+# EOF
+
