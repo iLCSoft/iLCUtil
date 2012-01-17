b9d97d9cc72875c1127bec661a4ca900791ddbc6
Jan Engels <jan.engels@desy.de>
2012-01-17 16:43:33 +0000

    updated versions

--- cmakemodules/ilcsoft_versions_v01-13-01.cmake
+++ cmakemodules/ilcsoft_versions_v01-13-01.cmake
@@ -4,12 +4,24 @@
 include( "ilcsoft_versions_v01-13" )
 
 # ===== external tools ================
-#set( geant4_version                 "9.4.p02"   )
+set( geant4_version                 "9.4.p03"   )
+set( clhep_version                  "2.1.0.1"   )
 
 # ============ ilcsoft core packages ================
-set( kaldet_version                 "branches/v01-04-pre00" )
-set( marlintrk_version              "branches/v01-02-pre00" )
-set( marlintrkprocessors_version    "branches/v01-01-pre00" )
+set( ced_version                    "tags/v01-04-01" )
+set( kaldet_version                 "tags/v01-04" )
+set( marlintrk_version              "tags/v01-02" )
+
+set( mokka_version                  "tags/mokka-07-07-p04" )
+
+# ============ ilcsoft marlin plugins ===============
+set( cedviewer_version              "tags/v01-04-01" )
+set( marlintrkprocessors_version    "tags/v01-01" )
+set( clupatra_version               "tags/v00-04" )
+
+
+# ============ ilcsoft config packages ==============
+set( mokkadbconfig_version          "tags/v03-05" )
 
 # EOF
 
