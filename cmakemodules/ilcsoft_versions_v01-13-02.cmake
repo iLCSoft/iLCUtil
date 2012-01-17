f9b96e49ad24965ce0cbc0dcf42e5e7808c0483b
Jan Engels <jan.engels@desy.de>
2012-01-17 16:46:59 +0000

    added v01-13-02 (still in preparation)

---
+++ cmakemodules/ilcsoft_versions_v01-13-02.cmake
@@ -0,0 +1,18 @@
+# package versions from the ilcsoft release v01-13-02
+# this file should not be changed!
+
+include( "ilcsoft_versions_v01-13-01" )
+
+# ============ ilcsoft core packages ================
+set( lcio_version                    "tags/v02-00-03" )
+set( mokka_version                  "tags/mokka-07-07-p04" )
+
+# ============ ilcsoft marlin plugins ===============
+set( forwardtracking_version        "tags/v01-01-01" )
+
+
+# ============ ilcsoft config packages ==============
+#set( mokkadbconfig_version          "tags/v03-05" )
+
+# EOF
+
