2b419122f471856e02460db9f3099ef55e6a0c5c
Remi Ete <remi.ete@gmail.com>
2019-03-19 00:41:05 +0100

    Removed log prefix in attributes

--- streamlog/source/include/streamlog/logmessage.h
+++ streamlog/source/include/streamlog/logmessage.h
@@ -17,13 +17,13 @@ namespace streamlog {
   struct logmessage {
     using time_point = std::chrono::system_clock::time_point ;
     using thread_id = std::thread::id ;
-    
+
     /// The logger name initiating the message
     std::string                 _loggerName { "UNKNOWN" } ;
     /// The log level publishing the message
-    unsigned int                _logLevel {0} ;
+    unsigned int                _level {0} ;
     /// The log level name publishing the message
-    std::string                 _logLevelName { "VERBOSE" } ;
+    std::string                 _levelName { "VERBOSE" } ;
     /// The thread id in which the message was generated
     thread_id                   _threadId { std::this_thread::get_id() } ;
     /// The time at which the message was published
