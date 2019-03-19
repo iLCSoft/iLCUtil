7c4898e64d2d982af6f3732655f19d802bd4b123
Remi Ete <remi.ete@gmail.com>
2019-03-19 01:20:42 +0100

    Added macros for logging in specific logger. Added aliases to global streamlog logger

--- streamlog/source/include/streamlog/streamlog.h
+++ streamlog/source/include/streamlog/streamlog.h
@@ -3,44 +3,44 @@
 #define streamlog_h
 
   /**
-   *  Main header file for the streamlog library. 
+   *  Main header file for the streamlog library.
    *  Includes necessary header files and defines macros <br>
    *      streamlog_out( MLEVEL )  <br>
    *      streamlog_message( MLEVEL , CODE_BLOCK , OUT_MESSAGE) <br>
-   * 
+   *
    *  @author F. Gaede, DESY
    *  @version $Id: streamlog.h,v 1.3 2007-09-12 11:59:53 gaede Exp $
    */
 
 #ifndef DONT_USE_STREAMLOG
-#define USE_STREAMLOG 
+#define USE_STREAMLOG
 #endif
 
 #ifdef  USE_STREAMLOG
 
-#include "logstream.h"
-#include "logscope.h"
-#include "loglevels.h"
+#include <streamlog/logstream.h">
+#include <streamlog/logscope.h>
+#include <streamlog/loglevels.h>
 
-
-#define  streamlog_level( MLEVEL ) ( streamlog::out.would_write< streamlog::MLEVEL >() )
-
-
-#define  streamlog_out( MLEVEL ) if(streamlog::out.write< streamlog::MLEVEL >() ) streamlog::out()  
-
-
-#define  streamlog_message( MLEVEL , CODE_BLOCK , OUT_MESSAGE)\
-   if( streamlog::out.write< streamlog::MLEVEL >() ) { \
+#define  streamlog_logger_level( LOGGER, MLEVEL ) ( (LOGGER).would_write< streamlog::MLEVEL >() )
+#define  streamlog_logger_out( LOGGER, MLEVEL ) if((LOGGER).write< streamlog::MLEVEL >() ) (LOGGER)
+#define  streamlog_logger_message( LOGGER, MLEVEL , CODE_BLOCK , OUT_MESSAGE)\
+   if( (LOGGER).write< streamlog::MLEVEL >() ) { \
       CODE_BLOCK \
-    streamlog::out() << OUT_MESSAGE }
-
+    (LOGGER) << OUT_MESSAGE }
 //for use in templated classes with gcc 3.2 use these macros:
-#define  streamlog_out_T( MLEVEL ) if(streamlog::out.template write< streamlog::MLEVEL >() ) streamlog::out() 
-
-#define  streamlog_message_T( MLEVEL , CODE_BLOCK , OUT_MESSAGE)\
-   if( streamlog::out.template write< streamlog::MLEVEL >() ) { \
+#define  streamlog_logger_out_T( LOGGER, MLEVEL ) if((LOGGER).template write< streamlog::MLEVEL >() ) (LOGGER)
+#define  streamlog_logger_message_T( LOGGER, MLEVEL , CODE_BLOCK , OUT_MESSAGE)\
+   if( (LOGGER).template write< streamlog::MLEVEL >() ) { \
       CODE_BLOCK \
-    streamlog::out() << OUT_MESSAGE }
+    (LOGGER) << OUT_MESSAGE }
+
+// alias to global logger
+#define  streamlog_level( MLEVEL ) streamlog_logger_level(streamlog::out, MLEVEL)
+#define  streamlog_out( MLEVEL ) streamlog_logger_out(streamlog::out, MLEVEL)
+#define  streamlog_message( MLEVEL , CODE_BLOCK , OUT_MESSAGE) streamlog_logger_message(streamlog::out, MLEVEL, CODE_BLOCK, OUT_MESSAGE)
+#define  streamlog_out_T( MLEVEL ) streamlog_logger_out_T(streamlog::out, MLEVEL)
+#define  streamlog_message_T( MLEVEL , CODE_BLOCK , OUT_MESSAGE) streamlog_logger_message_T(streamlog::out, MLEVEL, CODE_BLOCK, OUT_MESSAGE)
 
 #else
 
