5636fadb9cff0b23b1527df8b2d2915b199b902c
Remi Ete <remi.ete@gmail.com>
2019-03-21 17:56:06 +0100

    Updated logger type

--- example/streamlog_example.cc
+++ example/streamlog_example.cc
@@ -12,32 +12,27 @@ int main(int, char** argv) {
   // get and configure the global logger
   auto &main = streamlog::logstream::global() ;
   main.setName( "streamlog_test" ) ;
-  main.setLevel<DEBUG>() ;
-  
-  streamlog_out(ERROR)   << "ERROR   from main logger\nbibou" ;
-  
-  streamlog_out(DEBUG)   << "DEBUG   from main logger" << std::endl ;
-  streamlog_out(MESSAGE) << "MESSAGE from main logger" << "pouette" << std::endl ;
+  main.setLevel<MESSAGE>() ;
+
+  streamlog_out(DEBUG)   << "DEBUG from main logger" << std::endl ;
+  streamlog_out(MESSAGE) << "MESSAGE from main logger" << std::endl ;
   streamlog_out(WARNING) << "WARNING from main logger" << std::endl ;
-  streamlog_out(ERROR)   << "ERROR   from main logger" << std::endl ;
-  
+  streamlog_out(ERROR)   << "ERROR from main logger" << std::endl ;
+
   // try the colored console (multi threaded mode)
-  auto color_console = std::make_shared<streamlog::colored_console_sink_mt>(); 
-  streamlog::logstream logger ( "test_logger" , color_console ) ;
-  logger.setLevel<MESSAGE>() ;
-  
-  streamlog_logger_out(logger, MESSAGE) << "Is debug active ? ";
-  streamlog_logger_out(logger, MESSAGE) << (STREAMLOG_DEBUG_ACTIVE ? "yes" : "no") << std::endl ; 
-  
-  streamlog_logger_out(logger, DEBUG) << "DEBUG   from test logger" << std::endl ;
+  auto color_console = std::make_shared<streamlog::colored_console_sink_mt>();
+  streamlog::logstream_mt logger ( "test_logger" , color_console ) ;
+  logger.setLevel<DEBUG>() ;
+
+  streamlog_logger_out(logger, MESSAGE) << "Is debug active ? " << (STREAMLOG_DEBUG_ACTIVE ? "yes" : "no") << std::endl ;
+  streamlog_logger_out(logger, DEBUG)   << "DEBUG from test logger" << std::endl ;
   streamlog_logger_out(logger, MESSAGE) << "MESSAGE from test logger" << std::endl ;
   streamlog_logger_out(logger, WARNING) << "WARNING from test logger" << std::endl ;
-  streamlog_logger_out(logger, ERROR) << "ERROR   from test logger" << std::endl ;
-  streamlog_logger_out(logger, DEBUG) << "DEBUG   from test logger" << std::endl ;
-  streamlog_logger_out(logger, ERROR) << "ERROR   from test logger" << std::endl ;
-  
-  streamlog_out(ERROR)   << "ERROR   from main logger" << std::endl ;
+  streamlog_logger_out(logger, ERROR)   << "ERROR from test logger" << std::endl ;
+  streamlog_logger_out(logger, DEBUG)   << "DEBUG from test logger" << std::endl ;
+  streamlog_logger_out(logger, ERROR)   << "ERROR from test logger" << std::endl ;
+
+  streamlog_out(ERROR)   << "ERROR from main logger" << std::endl ;
 
   return 0;
 }
-
