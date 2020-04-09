f390232e79649e3ae5032457007d1dff5977851c
Frank Gaede <frank.gaede@desy.de>
2020-04-09 19:03:39 +0200

    replace logbuffer with printthread

--- streamlog/source/src/logstream.cc
+++ streamlog/source/src/logstream.cc
@@ -1,37 +1,13 @@
 #include "streamlog/logstream.h"
-
-#include "streamlog/logbuffer.h"
 #include "streamlog/prefix.h"
 
 namespace streamlog{
-  
-  logstream::logstream() : 
-    _ns( new nullstream ) , 
-    _os(0) , 
-    _level(0) ,
-    _active(false) , 
-    _lb(0),
-    _prefix( new streamlog::prefix) {
-    
-  } 
+
+ 
+  logstream::logstream() : _prefix( new streamlog::prefix ){} 
 
   logstream::~logstream() {
-    
-    if( _ns ){ 
-        delete _ns ;
-        _ns = NULL ;
-    }
-    
-    if( _os ){ 
-      delete _os ;
-      _os = NULL ;
-    }
-    
-    if( _lb ){
-      delete _lb ;
-      _lb = NULL ;
-    }
-    
+
     if( _prefix ){
       delete _prefix ;
       _prefix = NULL ; 
@@ -43,19 +19,9 @@ namespace streamlog{
     static bool first=true ;
     
     if( first && os ) {
-      
-      //      _name = name ;      
-      
-      // create a new log buffer and attach this to a wrapper to the given ostream  
 
-      _lb = new logbuffer( os.rdbuf() , this ) ;
-      
-      _os = new std::ostream( _lb ) ;
-      
-      //attach also the original stream to the logger...
-      //os.rdbuf( _lb ) ; // this needs some work 
-      // FIXME : this needs to go to the c'tor !!!!
-     
+      _os = &os ;
+
       _prefix->_name = name ;
 
       first = false ;
@@ -76,20 +42,21 @@ namespace streamlog{
     return l ;
   } 
 
-
-  std::ostream& logstream::operator()() { 
+  printthread logstream::operator()() { 
     
     if( _active && _os ) {
       
       _active = false ;
       
-      return *_os ;
+      return printthread{(*_prefix)(),_os} ;
     }
     else
-      return *_ns ;
-    
+     return printthread{} ;
   }
 
-  /** global instance of logstream */
+  /** thread global instance of logstream */
   thread_local logstream out ;
+
+  /// global mutex for actual io
+  std::mutex printthread::_mutexPrint{};
 }
