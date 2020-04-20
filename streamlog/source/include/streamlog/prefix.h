114d8a0b266855498d520aa09b52549ed2864238
Remi Ete <remi.ete@gmail.com>
2020-04-20 16:58:18 +0200

    Added documentation line

--- streamlog/source/include/streamlog/prefix.h
+++ streamlog/source/include/streamlog/prefix.h
@@ -7,6 +7,8 @@ namespace streamlog {
     
   using logprefix = std::function<std::string()> ;
   
+  /** Generate a simple prefix function based on a logger name and log level
+   */
   inline static logprefix std_prefix( std::string name, std::string level ) {
     return [name=std::move(name),level=std::move(level)](){
       return "[ " + level + " \"" + name + "\"] " ;
