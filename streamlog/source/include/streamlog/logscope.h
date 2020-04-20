ffecedeedf4e29a6f396546a563fb2508e374ad3
Remi Ete <remi.ete@gmail.com>
2020-04-20 16:55:59 +0200

    Modernized logscope class

--- streamlog/source/include/streamlog/logscope.h
+++ streamlog/source/include/streamlog/logscope.h
@@ -1,9 +1,8 @@
-#ifndef logscope_h
-#define logscope_h
+#pragma once
 
-#include "streamlog/logstream.h"
+#include <streamlog/logstream.h>
 
-namespace streamlog{
+namespace streamlog {
 
   /** Helper class to change the current scope name and log level - 
    *  if the object goes out of scope, e.g. at the end of the method
@@ -11,17 +10,15 @@ namespace streamlog{
    *  they had previously, i.e. before changed through this object.<br>
    *  Example:
    *  <pre>
-   *   streamlog::logscope scope( streamlog::out ) ;
-   *   scope.setName( "TrackFitter" ) ;
-   *   scope.setLevel< streamlog::MESSAGE3 >() ;
+   *   streamlog::logscope scope( streamlog::out, "TrackFitter", MESSAGE3::name() ) ;
    *  </pre>
    *  
    * 
    *  @author F. Gaede, DESY
+   *  @author R. Ete, DESY
    *  @version $Id: logscope.h,v 1.2 2007-07-13 11:09:04 gaede Exp $
    */
   class logscope{
-
   public:
     logscope() = delete ;
     logscope(const logscope&) = delete ;
@@ -29,49 +26,39 @@ namespace streamlog{
 
     /** Instantiate a scope object for the given logstream.
      */
-    logscope(logstream& ls) : _ls(&ls) , _name("") , _level(-1) { }
+    logscope( logstream& ls, const std::string &name, const std::string &ln ) : 
+      _logstream(ls), 
+      _oldName(ls.name()),
+      _oldLevel(ls.levelName()) {
+      _logstream.set_name( name ) ;
+      _logstream.set_level( ln ) ;
+    }
     
     /** Reset old name and level if set through this object.
      */
-    ~logscope(){
-
-      if( _name.size() > 0 ) {
-	_ls->prefix()->_name=_name ;
-	//std::cerr << "  ~logscope()  reset name to " << _name << std::endl ;
-	
-      }
-      if(  _level > -1 )
- 	_ls->setLevel( _level ) ; 
+    ~logscope() {
+      release() ;
     }
     
-    /** Change current log scope name for the lifetime of this object */
-    void setName(const std::string name) {
-      _name =  _ls->prefix()->_name ;
-      _ls->prefix()->_name = name ;
-    }
-  
-
-    /** Change current log level for the lifetime of this object */
-    template <class T>
-    void setLevel(){
-      _level =  _ls->_level ;
-      _ls->setLevel( T::level )  ;
-    }
-
-    /** Change current log level for the lifetime of this object 
-     *  through a string level name that has been registered with the logstream
-     *  via logstream::addLevelName - otherwise the call will have no effect.
+    /** Release the log scope, switch back to the old log scope
      */
-    void setLevel(const std::string& level){
-      _level =  _ls->setLevel( level )  ;
+    void release() {
+      if( not _released ) {
+        _logstream.set_name( _oldName ) ;
+        _logstream.set_level( _oldLevel ) ;
+        _released = true ;
+      }
     }
-   
-  protected: 
-    logstream* _ls = nullptr ;
-    std::string _name{} ;
-    long _level{};
     
+  protected:
+    /// The logstream on which to change the log scope
+    logstream               &_logstream ;
+    /// The old logstream name (cache)
+    const std::string        _oldName ;
+    /// The old logstream log level (cache)
+    const std::string        _oldLevel ;
+    /// Whether the log scope has been released
+    bool                     _released {false} ;
   };
   
 }
-#endif
