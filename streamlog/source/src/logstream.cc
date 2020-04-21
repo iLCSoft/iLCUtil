ac33bb10e6f0dfa94ff5ac9ea2997560715811ba
Remi Ete <remi.ete@gmail.com>
2020-04-21 16:57:43 +0200

    Fixed nullptr comparison

--- streamlog/source/src/logstream.cc
+++ streamlog/source/src/logstream.cc
@@ -48,10 +48,8 @@ namespace streamlog {
   }
   
   void logstream::init( std::ostream& os , const std::string &name ) {
-    if( nullptr != &os ) {
-      set_stream( &os ) ;
-      set_name( name ) ;
-    }
+    set_stream( &os ) ;
+    set_name( name ) ;
   }
 
   unsigned logstream::set_level( const std::string& levelName ) {
@@ -92,53 +90,4 @@ namespace streamlog {
     __logconfig__.apply( &out ) ;
   }
   
-  logstream::LevelMap logstream::builtin_levels() {
-    return {
-      {DEBUG::name(), DEBUG::level},
-      {DEBUG0::name(), DEBUG0::level},
-      {DEBUG1::name(), DEBUG1::level},
-      {DEBUG2::name(), DEBUG2::level},
-      {DEBUG3::name(), DEBUG3::level},
-      {DEBUG4::name(), DEBUG4::level},
-      {DEBUG5::name(), DEBUG5::level},
-      {DEBUG6::name(), DEBUG6::level},
-      {DEBUG7::name(), DEBUG7::level},
-      {DEBUG8::name(), DEBUG8::level},
-      {DEBUG9::name(), DEBUG9::level},
-      {MESSAGE::name(), MESSAGE::level},
-      {MESSAGE0::name(), MESSAGE0::level},
-      {MESSAGE1::name(), MESSAGE1::level},
-      {MESSAGE2::name(), MESSAGE2::level},
-      {MESSAGE3::name(), MESSAGE3::level},
-      {MESSAGE4::name(), MESSAGE4::level},
-      {MESSAGE5::name(), MESSAGE5::level},
-      {MESSAGE6::name(), MESSAGE6::level},
-      {MESSAGE7::name(), MESSAGE7::level},
-      {MESSAGE8::name(), MESSAGE8::level},
-      {MESSAGE9::name(), MESSAGE9::level},
-      {WARNING::name(), WARNING::level},
-      {WARNING0::name(), WARNING0::level},
-      {WARNING1::name(), WARNING1::level},
-      {WARNING2::name(), WARNING2::level},
-      {WARNING3::name(), WARNING3::level},
-      {WARNING4::name(), WARNING4::level},
-      {WARNING5::name(), WARNING5::level},
-      {WARNING6::name(), WARNING6::level},
-      {WARNING7::name(), WARNING7::level},
-      {WARNING8::name(), WARNING8::level},
-      {WARNING9::name(), WARNING9::level},
-      {ERROR::name(), ERROR::level},
-      {ERROR0::name(), ERROR0::level},
-      {ERROR1::name(), ERROR1::level},
-      {ERROR2::name(), ERROR2::level},
-      {ERROR3::name(), ERROR3::level},
-      {ERROR4::name(), ERROR4::level},
-      {ERROR5::name(), ERROR5::level},
-      {ERROR6::name(), ERROR6::level},
-      {ERROR7::name(), ERROR7::level},
-      {ERROR8::name(), ERROR8::level},
-      {ERROR9::name(), ERROR9::level},
-      {SILENT::name(), SILENT::level}
-    };
-  }
 }
