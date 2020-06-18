8d0f5ac98d6be7d5556327330fa86ced184852f1
Remi Ete <remi.ete@gmail.com>
2020-06-18 15:37:16 +0200

    Turned the prefix base+impl classes to a simple std function

--- streamlog/source/include/streamlog/prefix.h
+++ streamlog/source/include/streamlog/prefix.h
@@ -2,56 +2,20 @@
 #ifndef prefix_h
 #define prefix_h
 
-#include <sstream>
+#include <functional>
+#include <string>
 
-namespace streamlog{
-
-  class logscope ;
-  class logstream ;
-
-
-  /** Base class for log message prefix formating. Subclasses need to define 
-   *  std::string operator()() using this->_levelName and this->_name.
-   * 
-   *  @author F. Gaede, DESY
-   *  @version $Id: prefix.h,v 1.1.1.1 2007-07-12 17:14:48 gaede Exp $
-   */
-  class prefix_base{
-  
-    friend class logscope ;
-    friend class logstream ;
-
-  protected:
-    std::string _name{} ;
-    std::string _levelName{} ;
-  
-  public:
-    prefix_base() ;
-    virtual ~prefix_base() ;
- 
-    virtual std::string operator()() =0 ;
+namespace streamlog {
     
-    void setLevelName( const std::string& lName) { _levelName = lName ; } 
+  using logprefix = std::function<std::string()> ;
   
-  };
-
-  /** Default log message prefix:  [ LevelName "ScopeName"].
-   * 
-   *  @author F. Gaede, DESY
-   *  @version $Id: prefix.h,v 1.1.1.1 2007-07-12 17:14:48 gaede Exp $
+  /** Generate a simple prefix function based on a logger name and log level
    */
-
-  class prefix : public prefix_base{
-  public:
-    virtual std::string operator()()  {
-      std::stringstream ss ;
-      ss << "[ " << this->_levelName << " \"" << this->_name << "\"] " ;
-      return ss.str() ;
-    }
-
-  };
-
-
-
+  inline static logprefix std_prefix( std::string name, std::string level ) {
+    return [name=std::move(name),level=std::move(level)](){
+      return "[ " + level + " \"" + name + "\"] " ;
+    };
+  }
 }
+
 #endif
